79f47649f919bf2051b136a074702b133db5713d
Iqraa Manuel <iqraathemanuel@icloud.com>
2025-05-23 16:00:11 -0400

    Initial Commit

---
+++ SpotifySwitcherClean/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SpotifySwitcherClean
+//
+//  Created by Iqraa Manuel on 5/23/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
